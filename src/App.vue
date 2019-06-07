54405f17e4d214e89cb49316ab5a7643f51a6093
maoyeyang <673842307@qq.com>
2019-06-07 17:12:55 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <mt-header fixed
+               title="仿黑马程序员VUE项目"></mt-header>
+    <router-view />
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active"
+         href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item"
+         href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </a>
+      <a class="mui-tab-item"
+         href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-email"><span class="mui-badge">0</span></span>
+        <span class="mui-tab-label">购物车</span>
+      </a>
+      <a class="mui-tab-item"
+         href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">搜索</span>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+* {
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  padding-top: 40px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="lo"></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
